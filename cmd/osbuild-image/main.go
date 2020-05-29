ffe76f07851ca5e813444d753c26fad765c0d946
Ondřej Budai <obudai@redhat.com>
2020-05-29 14:15:01 +0200

    add basic flag validation

--- cmd/osbuild-image/main.go
+++ cmd/osbuild-image/main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/ondrejbudai/osbuild-image/internal/weldr-image"
 )
 
+type flags struct {
+	imageType string
+	imagePath string
+}
+
+func validateFlags(flags *flags) error {
+	// do not validate imageType, weldr_image can return all the possible values
+	if flags.imagePath == "" {
+		return errors.New("image path cannot be empty")
+	}
+	return nil
+}
+
 func main() {
-	imageType := flag.String("type", "", "image type to be built")
-	output := flag.String("output", "", "filename of the produced image")
+	var flags flags
+	flag.StringVar(&flags.imageType, "type", "", "image type to be built")
+	flag.StringVar(&flags.imagePath, "output", "", "filename of the produced image")
 	flag.Parse()
 
-	imageFile, err := os.Create(*output)
+	if err := validateFlags(&flags); err != nil {
+		fmt.Fprintf(os.Stderr, "validation of arguments failed: %v\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	imageFile, err := os.Create(flags.imagePath)
 
 	if err != nil {
 		log.Fatal("cannot open the output file", err)
 	}
 
 	req := &weldr_image.Request{
-		ImageType:   *imageType,
+		ImageType:   flags.imageType,
 		ImageWriter: imageFile,
 	}
 
